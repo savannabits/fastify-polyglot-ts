8d5720c5e783a1acb6583bc9a0237146d2eb5b6d
Sam Maosa <maosa.sam@gmail.com>
2021-11-08 10:50:56 +0300

    feat(index.ts): implemented te core functionality of the plugin in typescript
    - Added type definitions to support i18n
    - Added the core functionality of the plugin built upon node-polyglot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,78 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+import path from 'path';
+import { Dirent, readdirSync } from 'fs';
+import fp from 'fastify-plugin';
+import Polyglot from 'node-polyglot';
+import { ERR_MISSING_DICTIONARY_FOR_DEFAULT_LOCALE } from './errors';
+import { FastifyInstance, FastifyPluginOptions } from 'fastify';
+
+export declare class FastifyPolyglot extends Polyglot {
+  locales?: Record<string, any>;
+  defaultLocale?: string;
+}
+
+declare module 'fastify' {
+  export interface FastifyInstance {
+    i18n: FastifyPolyglot;
+  }
+}
+
+const ACCEPTED_EXTENSIONS = ['.js', '.ts', '.json'];
+
+export default fp(
+  (
+    fastify: FastifyInstance,
+    opts: FastifyPluginOptions,
+    next: (error?: Error | undefined) => void
+  ) => {
+    const getLocales = (basepath: string) => {
+      let contents: Array<Dirent> = [];
+      try {
+        contents = readdirSync(basepath, { withFileTypes: true });
+      } catch (err) {
+        fastify.log.warn(err);
+      }
+      return contents
+        .filter(entry => {
+          const ext = path.extname(entry.name);
+          return entry.isFile() && ACCEPTED_EXTENSIONS.indexOf(ext) !== -1;
+        })
+        .reduce((locales, entry) => {
+          const name = entry.name.replace(/\.[^/.]+$/, '');
+          const pathname = path.join(basepath, name);
+          locales[name] = require(pathname);
+          return locales;
+        }, {});
+    };
+
+    const mergeLocales = (a = {}, b = {}) => {
+      const locales = { ...a };
+      Object.keys(b).forEach(key => {
+        locales[key] = {
+          ...a[key],
+          ...b[key],
+        };
+      });
+      return locales;
+    };
+
+    const defaultLocale: string = opts.defaultLocale || 'en';
+    const localesPath: string = opts.localesPath || './locales';
+    const loadedLocales = getLocales(localesPath);
+    const locales = mergeLocales(opts.locales, loadedLocales);
+
+    if (Object.keys(locales).indexOf(defaultLocale) === -1) {
+      next(new Error(ERR_MISSING_DICTIONARY_FOR_DEFAULT_LOCALE));
+    }
+
+    const i18n: FastifyPolyglot = new Polyglot({
+      phrases: locales[defaultLocale],
+      locale: defaultLocale,
+    });
+
+    i18n.locales = locales;
+    i18n.defaultLocale = defaultLocale;
+
+    fastify.decorate('i18n', i18n);
+    next();
+  }
+);
